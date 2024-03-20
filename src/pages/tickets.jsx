68373eecd712409aeefd6077740a9b6cd347b1ab
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2024-03-20 12:10:24 -0500

    Front para captar datos y peticion para almacenamiento de tarjeta lista

--- src/pages/tickets.jsx
+++ src/pages/tickets.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useEffect, useState, useContext } from "react";
-import { checkNFT, getCurrentWalletConnected } from "../utils/interact.jsx";
+import { checkNFT, getCurrentWalletConnected, postCard } from "../utils/interact.jsx";
 import { Button, Box, Grid, CardActionArea, CardActions, Card, CardMedia, CardContent, Typography, Stack, TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import { AppContext } from '../App.jsx';
 
@@ -13,7 +13,18 @@ const Tickets = () => {
     //const [maxTickets, setMaxTickets] = React.useState(0);
     //const [ticketId, setTicketId] = React.useState(0);
     //const [ticketPrice, setTicketPrice] = React.useState(0);
-    const [ticketAmount, setTicketAmount] = React.useState(0);
+    const [cardNumber, setCardNumber] = React.useState(0);
+    const [CVV, setCVV] = React.useState(0);
+    const [expiryMonth, setExpiryMonth] = React.useState(0);
+    const [expiryYear, setExpiryYear] = React.useState(0);
+    const [fullName, setFullName] = React.useState(0);
+    const [addressLine, setAddressLine] = React.useState(0);
+    const [postalCode, setPostalCode] = React.useState(0);
+    const [city, setCity] = React.useState(0);
+    const [district, setDistrict] = React.useState(0);
+    const [country, setCountry] = React.useState(0);
+    const [phone, setPhone] = React.useState(0);
+    const [email, setEmail] = React.useState(0);
     var contextData = useContext(AppContext);
 
 
@@ -44,7 +55,7 @@ const Tickets = () => {
         //setMaxTickets(pMaxTickets)
         //setTicketId(pTicketID)
         //setTicketPrice(pTicketPrice)
-        console.log(pMaxTickets,pTicketID,pTicketPrice)
+        console.log(pMaxTickets, pTicketID, pTicketPrice)
         setOpen(true);
     };
 
@@ -55,18 +66,12 @@ const Tickets = () => {
     const handlepurchase = async () => {
         const { address } = await getCurrentWalletConnected()
         if (address !== undefined && address.length > 1) {
-            // missing max tickests
-            if (ticketAmount > 0 && ticketAmount <= 1) {
-                const { severity, status } = {severity:'success',status:'Test'}
+                let payLoad = { "cardNumer": cardNumber, "CVV": CVV, "expiryMonth": expiryMonth, "expiryYear": expiryYear, "fullName": fullName, "addressLine": addressLine, "postalCode": postalCode, "city": city, "district": district, "country": country, "phone": phone, "email": email }
+                postCard(payLoad)
+                const { severity, status } = { severity: 'success', status: 'Test' }
                 contextData.severity(severity)
                 contextData.text(status);
                 contextData.show(true)
-            }
-            else {
-                contextData.severity("warning")
-                contextData.text("Cantidad incorrecta");
-                contextData.show(true)
-            }
 
         }
         else {
@@ -79,12 +84,12 @@ const Tickets = () => {
     };
 
     return (
-        <Grid container justifyContent="center"  maxWidth="xl" sx={{ display: 'flex', alignItems: 'center', bgcolor: '#cfe8fc', minHeight: '80vh', borderRadius: 1, mt: 3, background: 'linear-gradient(to bottom, #F8F8F8, #FFFFFF)' }}>
+        <Grid container justifyContent="center" maxWidth="xl" sx={{ display: 'flex', alignItems: 'center', bgcolor: '#cfe8fc', minHeight: '80vh', borderRadius: 1, mt: 3, background: 'linear-gradient(to bottom, #F8F8F8, #FFFFFF)' }}>
             <Stack spacing={2} minWidth="90vw" justifyContent="center" alignItems="center">
-                <Typography sx={{ mt: 3, typography: {xs:'h5', sm: 'h5', md:'h3', lg:'h3'  } }}>Eventos</Typography>
+                <Typography sx={{ mt: 3, typography: { xs: 'h5', sm: 'h5', md: 'h3', lg: 'h3' } }}>Eventos</Typography>
                 <Box display="flex" alignItems="center">
                     <Grid container rowSpacing={1} maxWidth="xl" justifyContent="center" columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    {data?.map((nft) => (
+                        {data?.map((nft) => (
                             <Grid item key={nft['tokenId']}>
                                 <Card sx={{ maxWidth: 225, height: '100%' }}>
                                     <CardActionArea>
@@ -125,26 +130,167 @@ const Tickets = () => {
                                         <Dialog open={open} onClose={handleClose}>
                                             <DialogTitle>Compra de boletas</DialogTitle>
                                             <DialogContent>
-                                                <DialogContentText>
-                                                    ¿Cuántas boletas desea comprar?
-                                                </DialogContentText>
-                                                
-                                                    <TextField
-                                                        autoFocus
-                                                        margin="dense"
-                                                        type="number"
-                                                        fullWidth
-                                                        variant="standard"
-                                                        onChange={(event) => setTicketAmount(event.target.value)}
-                                                        InputProps={{
-                                                            inputProps: {
-                                                                type: 'number',
-                                                                // Missing max tickets
-                                                                min: 0, max: 1,
-                                                            },
-                                                        }}
-                                                    />
-                                               
+                                                <Grid container >
+                                                    <Grid xs={5.5}>
+                                                        <DialogContentText>
+                                                            Numero de tarjeta
+                                                        </DialogContentText>
+
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            type="number"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setCardNumber(event.target.value)}
+                                                            InputProps={{
+                                                                inputProps: {
+                                                                    type: 'number',
+                                                                },
+                                                            }}
+                                                        />
+                                                        <DialogContentText>
+                                                            Mes de expiración
+                                                        </DialogContentText>
+
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            type="number"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setExpiryMonth(event.target.value)}
+                                                            InputProps={{
+                                                                inputProps: {
+                                                                    type: 'number',
+                                                                },
+                                                            }}
+                                                        />
+                                                        <DialogContentText>
+                                                            Nombre completo
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setFullName(event.target.value)}
+                                                        />
+                                                        <DialogContentText>
+                                                            Código Postal
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            type="number"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setPostalCode(event.target.value)}
+                                                            InputProps={{
+                                                                inputProps: {
+                                                                    type: 'number',
+                                                                },
+                                                            }}
+                                                        />
+                                                        <DialogContentText>
+                                                            Estado
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setDistrict(event.target.value)}
+                                                        />
+                                                        <DialogContentText>
+                                                            Teléfono
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setPhone(event.target.value)}
+                                                        />
+                                                    </Grid>
+                                                    <Grid xs={1}></Grid>
+                                                    <Grid xs={5.5}>
+                                                        <DialogContentText>
+                                                            CVV
+                                                        </DialogContentText>
+
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            type="number"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setCVV(event.target.value)}
+                                                            InputProps={{
+                                                                inputProps: {
+                                                                    type: 'number',
+                                                                },
+                                                            }}
+                                                        />
+                                                        <DialogContentText>
+                                                            Año de expiración
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            type="number"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setExpiryYear(event.target.value)}
+                                                            InputProps={{
+                                                                inputProps: {
+                                                                    type: 'number',
+                                                                },
+                                                            }}
+                                                        />
+                                                        <DialogContentText>
+                                                            Dirección
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setAddressLine(event.target.value)}
+                                                        />
+                                                        <DialogContentText>
+                                                            Ciudad
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setCity(event.target.value)}
+                                                        />
+                                                        <DialogContentText>
+                                                            País
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setCountry(event.target.value)}
+                                                        />
+                                                        <DialogContentText>
+                                                            Email
+                                                        </DialogContentText>
+                                                        <TextField
+                                                            autoFocus
+                                                            margin="dense"
+                                                            fullWidth
+                                                            variant="standard"
+                                                            onChange={(event) => setEmail(event.target.value)}
+                                                        />
+                                                    </Grid>
+                                                </Grid>
+
                                             </DialogContent>
                                             <DialogActions>
                                                 <Button onClick={handleClose}>Cancelar</Button>
--- src/utils/interact.jsx
+++ src/utils/interact.jsx
@@ -1,5 +1,8 @@
 
 import { Alchemy, Network } from "alchemy-sdk";
+import { v4 as uuidv4 } from 'uuid';
+//import * as openpgp from 'openpgp';
+
 
 const alchemyKey = 'MZOFtJq4vlHU3MlvX7nCWJjtvEijsfDw';
 const config = {
@@ -109,3 +112,58 @@ export const checkNFT = async () => {
     }
   }
 }
+export const postCard = async (payLoad) => {
+  
+  const  pk =  await getPublicKey()
+  const url = 'https://api-sandbox.circle.com/v1/cards';
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: 'Bearer SAND_API_KEY:9783c9ab8e080ffe216e638f3801134d:90c3892d3eb3bd9a789989d2b370ede2'
+    },
+    body: JSON.stringify({
+      idempotencyKey: uuidv4(),
+      keyId: pk.data.keyId,
+      encryptedData: 'LS0tLS1CRUdJTiBQR1AgTUVTU0FHRS0tLS0tCgp3Y0JNQTBYV1NGbEZScFZoQVFmL2J2bVVkNG5LZ3dkbExKVTlEdEFEK0p5c0VOTUxuOUlRUWVGWnZJUWEKMGgzQklpRFNRU0RMZmI0NEs2SXZMeTZRbm54bmFLcWx0MjNUSmtPd2hGWFIrdnNSMU5IbnVHN0lUNWJECmZzeVdleXlNK1JLNUVHV0thZ3NmQ2tWamh2NGloY29xUnlTTGtJbWVmRzVaR0tMRkJTTTBsTFNPWFRURQpiMy91eU1zMVJNb3ZiclNvbXkxa3BybzUveWxabWVtV2ZsU1pWQlhNcTc1dGc1YjVSRVIraXM5ckc0cS8KMXl0M0FOYXA3UDhKekFhZVlyTnVNZGhGZFhvK0NFMC9CQnN3L0NIZXdhTDk4SmRVUEV0NjA5WFRHTG9kCjZtamY0YUtMQ01xd0RFMkNVb3dPdE8vMzVIMitnVDZKS3FoMmtjQUQyaXFlb3luNWcralRHaFNyd3NKWgpIdEphQWVZZXpGQUVOaFo3Q01IOGNsdnhZVWNORnJuNXlMRXVGTkwwZkczZy95S3loclhxQ0o3UFo5b3UKMFVxQjkzQURKWDlJZjRBeVQ2bU9MZm9wUytpT2lLall4bG1NLzhlVWc3OGp1OVJ5T1BXelhyTzdLWTNHClFSWm8KPXc1dEYKLS0tLS1FTkQgUEdQIE1FU1NBR0UtLS0tLQo',
+      billingDetails: {
+        name: payLoad.fullName,
+        city: payLoad.city,
+        country: payLoad.country,
+        line1: payLoad.addressLine,
+        district: payLoad.district,
+        postalCode: payLoad.postalCode
+      },
+      expMonth: payLoad.expiryMonth,
+      expYear: payLoad.expiryYear,
+      metadata: {
+        email: payLoad.email,
+        phoneNumber: payLoad.phone,
+        sessionId: 'DE6FA86F60BB47B379307F851E238617',
+        ipAddress: '244.28.239.130'
+      }
+    })
+  };
+
+  fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+    
+}
+
+const getPublicKey = async () => {
+
+  const url = 'https://api-sandbox.circle.com/v1/encryption/public';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Bearer SAND_API_KEY:9783c9ab8e080ffe216e638f3801134d:90c3892d3eb3bd9a789989d2b370ede2'
+    }
+  };
+
+  const resp = await fetch(url, options)
+  return await resp.json()
+} 
